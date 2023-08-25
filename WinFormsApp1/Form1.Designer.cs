293a39ff6d6d864a8c0c67cdd8d36e8f40f54d9a
aesys <aesys@YP-049>
2023-08-25 08:35:57 +0500

    Добавьте файлы проекта.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            textBox2 = new TextBox();
+            checkBox3 = new CheckBox();
+            textBox3 = new TextBox();
+            checkBox4 = new CheckBox();
+            textBox4 = new TextBox();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            IP = new DataGridViewTextBoxColumn();
+            Username = new DataGridViewTextBoxColumn();
+            PC = new DataGridViewTextBoxColumn();
+            MotherBoard = new DataGridViewTextBoxColumn();
+            CPU = new DataGridViewTextBoxColumn();
+            GPU = new DataGridViewTextBoxColumn();
+            RAM = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(83, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "192";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Checked = true;
+            checkBox1.CheckState = CheckState.Checked;
+            checkBox1.Location = new Point(12, 41);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 4;
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += check;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Checked = true;
+            checkBox2.CheckState = CheckState.Checked;
+            checkBox2.Location = new Point(101, 41);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(15, 14);
+            checkBox2.TabIndex = 6;
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += check;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(101, 12);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(83, 23);
+            textBox2.TabIndex = 5;
+            textBox2.Text = "168";
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Checked = true;
+            checkBox3.CheckState = CheckState.Checked;
+            checkBox3.Location = new Point(190, 41);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(15, 14);
+            checkBox3.TabIndex = 8;
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += check;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(190, 12);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(83, 23);
+            textBox3.TabIndex = 7;
+            textBox3.Text = "100";
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(279, 41);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(15, 14);
+            checkBox4.TabIndex = 10;
+            checkBox4.UseVisualStyleBackColor = true;
+            checkBox4.CheckedChanged += check;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(279, 12);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(83, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { IP, Username, PC, MotherBoard, CPU, GPU, RAM });
+            dataGridView1.Location = new Point(12, 77);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.ScrollBars = ScrollBars.Vertical;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(1254, 405);
+            dataGridView1.TabIndex = 11;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(415, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 43);
+            button1.TabIndex = 12;
+            button1.Text = "Check";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1001, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 13;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(593, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(170, 43);
+            button2.TabIndex = 14;
+            button2.Text = "ToExcel";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // IP
+            // 
+            IP.HeaderText = "IP";
+            IP.Name = "IP";
+            IP.ReadOnly = true;
+            // 
+            // Username
+            // 
+            Username.HeaderText = "Username";
+            Username.Name = "Username";
+            Username.ReadOnly = true;
+            // 
+            // PC
+            // 
+            PC.HeaderText = "PC";
+            PC.Name = "PC";
+            PC.ReadOnly = true;
+            // 
+            // MotherBoard
+            // 
+            MotherBoard.HeaderText = "MotherBoard";
+            MotherBoard.Name = "MotherBoard";
+            MotherBoard.ReadOnly = true;
+            // 
+            // CPU
+            // 
+            CPU.HeaderText = "CPU";
+            CPU.Name = "CPU";
+            CPU.ReadOnly = true;
+            // 
+            // GPU
+            // 
+            GPU.HeaderText = "GPU";
+            GPU.Name = "GPU";
+            GPU.ReadOnly = true;
+            // 
+            // RAM
+            // 
+            RAM.HeaderText = "RAM";
+            RAM.Name = "RAM";
+            RAM.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1278, 494);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(checkBox4);
+            Controls.Add(textBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(textBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(textBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private TextBox textBox2;
+        private CheckBox checkBox3;
+        private TextBox textBox3;
+        private CheckBox checkBox4;
+        private TextBox textBox4;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private DataGridViewTextBoxColumn IP;
+        private DataGridViewTextBoxColumn Username;
+        private DataGridViewTextBoxColumn PC;
+        private DataGridViewTextBoxColumn MotherBoard;
+        private DataGridViewTextBoxColumn CPU;
+        private DataGridViewTextBoxColumn GPU;
+        private DataGridViewTextBoxColumn RAM;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Enabled = checkBox1.Checked;
+            textBox2.Enabled = checkBox2.Checked;
+            textBox3.Enabled = checkBox3.Checked;
+            textBox4.Enabled = checkBox4.Checked;
+            thrad = new Thread(delegate () {
+                doSmth();
+            });
+        }
+
+        private void check(object sender, EventArgs e)
+        {
+            textBox1.Enabled = checkBox1.Checked;
+            if (!checkBox1.Checked) textBox1.Text = "";
+            textBox2.Enabled = checkBox2.Checked;
+            if (!checkBox2.Checked) textBox2.Text = "";
+            textBox3.Enabled = checkBox3.Checked;
+            if (!checkBox3.Checked) textBox3.Text = "";
+            textBox4.Enabled = checkBox4.Checked;
+            if (!checkBox4.Checked) textBox4.Text = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        public static bool ExecuteWithTimeLimit(TimeSpan timeSpan, System.Action codeBlock)
+        {
+            try
+            {
+                Task task = Task.Factory.StartNew(() => codeBlock());
+                task.Wait(timeSpan);
+                return task.IsCompleted;
+            }
+            catch (AggregateException ae)
+            {
+                //throw ae.InnerExceptions[0];
+                return false;
+            }
+        }
+        private string RemoveFirstWord(string line)
+        {
+            string[] t = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string res = "";
+            for (int i = 1; i < t.Length; i++)
+                res += t[i] + " ";
+            return res;
+        }
+        private void doSmth()
+        {
+
+            int flag;
+            if (!int.TryParse(textBox1.Text, out flag) && checkBox1.Checked) return;
+            if (!int.TryParse(textBox2.Text, out flag) && checkBox2.Checked) return;
+            if (!int.TryParse(textBox3.Text, out flag) && checkBox3.Checked) return;
+            if (!int.TryParse(textBox4.Text, out flag) && checkBox4.Checked) return;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+            string[] res = new string[7];
+            string[] argus = { "", "/NODE:%s computersystem get username","/NODE:%s computersystem get name", "/NODE:%s baseboard get product" , "/NODE:%s cpu get name",
+                "/NODE:%s path win32_videocontroller get Caption", "/NODE:%s MEMORYCHIP get partnumber" };
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "wmic.exe",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true
+                }
+            };
+            for (int i1 = 0; i1 < 255; i1++)
+            {
+                if (checkBox1.Checked) i1 = int.Parse(textBox1.Text);
+                for (int i2 = 0; i2 < 255; i2++)
+                {
+                    if (checkBox2.Checked) i2 = int.Parse(textBox2.Text);
+                    for (int i3 = 0; i3 < 255; i3++)
+                    {
+                        if (checkBox3.Checked) i3 = int.Parse(textBox3.Text);
+                        for (int i4 = 0; i4 < 255; i4++)
+                        {
+                            if (checkBox4.Checked) i4 = int.Parse(textBox4.Text);
+                            res[0] = $"{i1}.{i2}.{i3}.{i4}";
+                            label1.Text = res[0];
+                            for (int i = 1; i < 7; i++)
+                            {
+                                string line = "";
+                                proc.StartInfo.Arguments = argus[i].Replace("%s", res[0]);
+                                bool Completed = ExecuteWithTimeLimit(TimeSpan.FromMilliseconds(500), () =>
+                                {
+                                    proc.Start();
+                                    proc.WaitForExit();
+                                });
+                                if (Completed)
+                                {
+                                    while (!proc.StandardOutput.EndOfStream)
+                                    {
+                                        line += proc.StandardOutput.ReadLine();
+                                    }
+                                    res[i] = RemoveFirstWord(line);
+                                }
+
+                            }
+                            if (res[3] != "" && res[3] != null)
+                                this.dataGridView1.Rows.Add(res);
+                            for (int i = 0; i < res.Length; i++)
+                            {
+                                res[i] = null;
+                            }
+                            if (checkBox4.Checked) break;
+                        }
+                        if (checkBox3.Checked) break;
+                    }
+                    if (checkBox2.Checked) break;
+                }
+                if (checkBox1.Checked) break;
+            }
+            button1.Enabled = true;
+            try
+            {
+                thrad.Abort();
+            }
+            catch (Exception)
+            {
+            }
+            
+        }
+        bool isSearching = false;
+        Thread thrad;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            thrad.Start();
+        }
+
+        private void copyAlltoClipboard()
+        {
+            dataGridView1.SelectAll();
+            DataObject dataObj = dataGridView1.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occurred while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (*.xls)|*.xls";
+            sfd.FileName = DateTime.Now + ".xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                copyAlltoClipboard();
+                object misValue = System.Reflection.Missing.Value;
+                Excel.Application xlexcel = new Excel.Application();
+                xlexcel.DisplayAlerts = false;
+                Excel.Workbook xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+                Excel.Range rng = xlWorkSheet.get_Range("D:D").Cells;
+                rng.NumberFormat = "@";
+                Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
+                CR.Select();
+                xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+                Excel.Range delRng = xlWorkSheet.get_Range("A:A").Cells;
+                delRng.Delete(Type.Missing);
+                xlWorkSheet.get_Range("A1").Select();
+                xlWorkBook.SaveAs(sfd.FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlexcel.DisplayAlerts = true;
+                xlWorkBook.Close(true, misValue, misValue);
+                xlexcel.Quit();
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlexcel);
+                Clipboard.Clear();
+                dataGridView1.ClearSelection();
+            }
+        }
+    }
+}
